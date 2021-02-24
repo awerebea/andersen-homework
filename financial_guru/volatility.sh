f5384efff604aae655f96c4c9f2194afad966fb3
awerebea (pc-home) <awerebea.21@gmail.com>
2021-02-24 11:16:26 +0300

    Task02: Add arguments, errors handling, help
    Add arguments handling (month, min_year, db_path, help).
    Add validations of arguments and default error message if fails.
    Add check db file for existence and download it if needed.

--- financial_guru/volatility.sh
+++ financial_guru/volatility.sh
@@ -1,17 +1,109 @@
 #! /bin/bash
 
 # define "help" message
+help=$'usage: ./volatility.sh [OPTIONS] [DATABASE]
+Download the DATABASE if the file does not exist in the path.
+Show the value of the minimum volatility of the euro for the selected MONTH for
+years in the range starting from YEAR.
+Examples:
+./volatility.sh -m 03 -2015 quotes.json
+./volatility.sh --year 2017 --month 11 \'../quotes.json\'
 
-# difine conditions
-db_name="quotes.json"   # name of JSON database file
-min_year=2015           # start year of statistical calculations
-month=03                # select month
+DATABASE: relative path of DATABASE file being processed. Default "quotes.json"
+
+OPTIONS:
+  -m, --month MM    month in MM format. Default 03
+  -y, --year NUM    year in YYYY format. Default 2015
+
+  -h, --help        print this help message'
+
+# acceptable OPTIONS
+options=("-m --month -y --year")
+
+# define syntax error message
+error="Syntax error. Type './volatility.sh --help' to see more info"
+
+# difine default conditions
+def_db_path="quotes.json"   # name of JSON database file
+def_min_year=2015           # start year of statistical calculations
+def_month=03                # select month
+
+# parse input parameters
+# set default values to avoid errors if no arguments passed
+db_path=${def_db_path}
+min_year=${def_min_year}
+month=${def_month}
+# check count of arguments
+if [ "$#" -eq 1 ]; then
+# there is only one argument
+  if [[ $1 == '-h' || $1 == '--help' ]]; then
+    # the ony argument is 'help'
+    echo "$help"
+    exit 0
+  else
+    # the ony argument is db_path
+    db_path=$1
+  fi
+elif [ "$#" -gt 1 ]; then
+# args num > 1, store arguments in a array
+  args=("$@")
+  prev_arg_is_key=false
+  ind=0
+  for (( i=0;i<$#;i++)); do
+  # loop for each argument
+    if [ "$prev_arg_is_key" = true ]; then
+    # previous arg is option identifer
+      case "${args[${ind} - 1]}" in
+        -m|--month)
+          if [[ ${args[${ind}]} =~ ^[0-9]+$ && ${#args[${ind}]} -eq 2 ]];
+          then
+            month="${args[${ind}]}"
+          else
+            echo "$error"
+            exit 1
+          fi
+          ((ind++));;
+        -y|--year)
+          if [[ ${args[${ind}]} =~ ^[0-9]+$ && ${#args[${ind}]} -eq 4 ]];
+          then
+            min_year="${args[${ind}]}"
+          else
+            echo "$error"
+            exit 1
+          fi
+          ((ind++));;
+      esac
+      prev_arg_is_key=false
+    else
+    # previous arg is option value (or current arg is the first)
+      if [[ " ${options[@]} " =~ "${args[${ind}]}" ]]; then
+      # the arg is acceptable option key
+        prev_arg_is_key=true
+      else
+        if [ $ind -eq $(($#-1)) ]; then
+        # last arg may be db_path
+          proc="${args[${ind}]}"
+        else
+        # arg is not last in args - syntax error
+          echo "$error"
+          exit 1
+        fi
+      fi
+      ((ind++))
+    fi
+  done
+fi
+
+# check if db file exist and download it if not
+if [[ ! -f "$db_path" ]]; then
+  curl -s https://yandex.ru/news/quotes/graph_2000.json > $db_path
+fi
 
 # create table with raw 'jq' output
 raw_table=()
 while IFS= read -r line; do
   raw_table+=( "$line" )
-done < <( jq -r '.prices[][]' $db_name )
+done < <( jq -r '.prices[][]' $db_path )
 
 # DEBUG print
 # printf '%s\n' "${raw_table[@]}"
