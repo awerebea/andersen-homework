7456c908059bd9959ad65f4d242ac3f7d42da112
awerebea (pc-home) <awerebea.21@gmail.com>
2021-02-28 13:12:32 +0300

    Task02: Add option to define output format
    Add the '-o, --oneline' option to define the output as one line (only
    the line with the lowest volatility value will be printed).
    The default output with no '-o, --oneline' parameters specified in the
    arguments will print a table with values for each year.

--- financial_guru/volatility.sh
+++ financial_guru/volatility.sh
@@ -8,12 +8,15 @@ years in the range starting from YEAR.
 Examples:
 ./volatility.sh -m 3 -y 2015 quotes.json
 ./volatility.sh --year 2017 --month 11 \'../quotes.json\'
+./volatility.sh -y 2014 -m 3 --oneline
 
 DATABASE: relative path of DATABASE file being processed. Default "quotes.json"
 
 OPTIONS:
   -m, --month NUM   month in num [0]1-12 format. Default 3
   -y, --year NUM    year in YYYY format. Default 2015
+  -o, --oneline     print result as single line with lowest volatility value.
+                    Default 0
 
   -h, --help        print this help message'
 
@@ -27,12 +30,14 @@ error="Syntax error. Type './volatility.sh --help' to see more info"
 def_db_path="quotes.json"   # name of JSON database file
 def_min_year=2015           # start year of statistical calculations
 def_month=03                # select month
+def_oneline=0               # print whole table
 
 # parse input parameters
 # set default values to avoid errors if no arguments passed
-db_path=${def_db_path}
-min_year=${def_min_year}
-month=${def_month}
+db_path=$def_db_path
+min_year=$def_min_year
+month=$def_month
+oneline=$def_oneline
 # check count of arguments
 if [ "$#" -eq 1 ]; then
 # there is only one argument
@@ -40,6 +45,8 @@ if [ "$#" -eq 1 ]; then
     # the ony argument is 'help'
     echo "$help"
     exit 0
+  elif [[ $1 == '-o' || $1 == '--oneline' ]]; then
+    oneline=1
   else
     # the ony argument is db_path
     db_path=$1
@@ -75,8 +82,10 @@ elif [ "$#" -gt 1 ]; then
       esac
       prev_arg_is_key=false
     else
-    # previous arg is option value (or current arg is the first)
-      if [[ " ${options[@]} " =~ "${args[${ind}]}" ]]; then
+    # previous arg is option value (or current arg is the first or "--oneline")
+      if [[ ${args[${ind}]} == '-o' || ${args[${ind}]} == '--oneline' ]]; then
+        oneline=1
+      elif [[ " ${options[@]} " =~ "${args[${ind}]}" ]]; then
       # the arg is acceptable option key
         prev_arg_is_key=true
       else
@@ -145,57 +154,63 @@ done < <(printf '%s\n' "${table[@]}'" | awk '{print substr($1,2,4)}' | sort -u)
 # DEBUG print
 # printf '%s\n' "${years[@]}"
 
-# # calculate pair 'year volatility' with min volatility
-# min_volatility_pair=$( for year in "${years[@]}"; do
-#   stddev_tmp=$(printf '%s\n' "${table[@]}'" | \
-#     awk "/\"$year-$month/ {print \$2}")
-#       if  [[ ! -z $stddev_tmp ]]; then
-#         volatility=$(printf '%s\n' "$stddev_tmp'" | \
-#           awk '{x+=$0;y+=$0^2}END{print sqrt(y/NR-(x/NR)^2)*sqrt(252/12)}' | \
-#           awk '{printf "%.4f\n", $1}')
-#         printf '%s %s\n' "$year" "$volatility"
-#       fi
-#     done | LC_ALL=C sort -rg -k2 | tail -n 1 )
-# # DEBUG print
-# # echo $min_volatility_pair
-
-# # final output
-# printf "The %s with min volatility (%s) was in %s\n" \
-#   $(LC_ALL=us_EN.utf8 date -d "$month/01" +"%B") \
-#   $(echo $min_volatility_pair | awk '{printf $2}') \
-#   $(echo $min_volatility_pair | awk '{printf $1}')
-
-# calculate table with pairs 'year volatility'
-volatility_table=()
-for year in "${years[@]}"; do
-  stddev_tmp=$(printf '%s\n' "${table[@]}'" | \
-    awk "/\"$year-$month/ {print \$2}")
-      if  [[ ! -z $stddev_tmp ]]; then
-        volatility=$(printf '%s\n' "$stddev_tmp'" | \
-          awk '{x+=$0;y+=$0^2}END{print sqrt(y/NR-(x/NR)^2)*sqrt(252/12)}' | \
-          awk '{printf "%.4f\n", $1}')
-        volatility_table+=( "$year $volatility" )
-        if [ $year -eq $min_year ]; then
-          volatility_min=$volatility
-          volatility_min_year=$year
-        elif [ $(bc <<< "$volatility < $volatility_min") -eq 1 ]; then
-          volatility_min=$volatility
-          volatility_min_year=$year
+# the calculations below depend on whether the "--oneline" option is defined
+if  [[ $oneline -eq 1 ]]; then
+  # calculate pair 'year volatility' with min volatility
+  min_volatility_pair=$( for year in "${years[@]}"; do
+    filtered_data=$(printf '%s\n' "${table[@]}'" | \
+      awk "/\"$year-$month/ {print \$2}")
+        if  [[ ! -z $filtered_data ]]; then
+          volatility=$(printf '%s\n' "$filtered_data'" | \
+            awk '{x+=$0;y+=$0^2}END{print sqrt(y/NR-(x/NR)^2)*sqrt(252/12)}' | \
+            awk '{printf "%.4f\n", $1}')
+          printf '%s %s\n' "$year" "$volatility"
         fi
-      fi
-done
-# DEBUG print
-# printf '%s\n' "${volatility_table[@]}"
+      done | LC_ALL=C sort -rg -k2 | tail -n 1 )
+  # DEBUG print
+  # echo $min_volatility_pair
+else
+  # calculate table with pairs 'year volatility'
+  volatility_table=()
+  for year in "${years[@]}"; do
+    filtered_data=$(printf '%s\n' "${table[@]}'" | \
+      awk "/\"$year-$month/ {print \$2}")
+        if  [[ ! -z $filtered_data ]]; then
+          volatility=$(printf '%s\n' "$filtered_data'" | \
+            awk '{x+=$0;y+=$0^2}END{print sqrt(y/NR-(x/NR)^2)*sqrt(252/12)}' | \
+            awk '{printf "%.4f\n", $1}')
+          volatility_table+=( "$year $volatility" )
+          if [[ -z $volatility_min ]]; then
+            volatility_min=$volatility
+            volatility_min_year=$year
+          elif [ $(bc <<< "$volatility < $volatility_min") -eq 1 ]; then
+            volatility_min=$volatility
+            volatility_min_year=$year
+          fi
+        fi
+  done
+fi
+  # DEBUG print
+  # printf '%s\n' "${volatility_table[@]}"
 
-# final output
-printf "Volatility for $(tput bold)$(tput setaf 3)%s$(tput sgr0) by years:\n" \
-  $(LC_ALL=us_EN.utf8 date -d "$month/01" +"%B")
-for line in "${volatility_table[@]}"; do
-  if [ $(echo $line | awk '{print $1}') -eq $volatility_min_year ]; then
-    printf "$(tput bold)$(tput setaf 2)%s - %s$(tput sgr0)\n" \
-      $(echo $line | awk '{printf $1}') $(echo $line | awk '{printf $2}')
-  else
-    printf "%s - %s\n" \
-      $(echo $line | awk '{printf $1}') $(echo $line | awk '{printf $2}')
-  fi
-done
+# final output depend on whether the "--oneline" option is defined
+if  [[ $oneline -eq 1 ]]; then
+  printf "The $(tput bold)%s$(tput sgr0) with min Volatility \
+($(tput bold)$(tput setaf 2)%s$(tput sgr0)) was in \
+$(tput bold)$(tput setaf 2)%s$(tput sgr0)\n" \
+    $(LC_ALL=us_EN.utf8 date -d "$month/01" +"%B") \
+    $(echo $min_volatility_pair | awk '{printf $2}') \
+    $(echo $min_volatility_pair | awk '{printf $1}')
+else
+  printf "Volatility for $(tput bold)%s$(tput sgr0) by years:\n" \
+    $(LC_ALL=us_EN.utf8 date -d "$month/01" +"%B")
+  for line in "${volatility_table[@]}"; do
+    if [ $(echo $line | awk '{print $1}') -eq $volatility_min_year ]; then
+      printf "$(tput bold)$(tput setaf 2)$(tput smso)%s - %s$(tput sgr0)\n" \
+        $(echo $line | awk '{printf $1}') $(echo $line | awk '{printf $2}')
+    else
+      printf "%s - %s\n" \
+        $(echo $line | awk '{printf $1}') $(echo $line | awk '{printf $2}')
+    fi
+  done
+fi
